27e021d6138286d063cbda7aa579f45cf8a58820
SanjanimoCarpe <workaholicjani77@gmail.com>
2026-09-07 21:52:09 +0700

    Update application URL to production environment

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://setara-production-0557.up.railway.app'),
 
     /*
     |--------------------------------------------------------------------------
